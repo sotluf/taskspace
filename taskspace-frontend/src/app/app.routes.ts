228c9c4e26097e683c49e707f7228ca28196d891
Oleksandra Maryanko <maryankoaleksandra@gmail.com>
2026-05-13 12:53:33 +0200

    feat: configure routes for components

--- taskspace-frontend/src/app/app.routes.ts
+++ taskspace-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Board } from './kanban/board/board';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'board', pathMatch: 'full' },
+  { path: 'board', component: Board },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+];
