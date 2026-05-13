2c024ac5ce03c239c5c0daab58038bd75dc34294
Oleksandra Maryanko <maryankoaleksandra@gmail.com>
2026-05-13 12:55:49 +0200

    feat: add board component with logic and styles

--- taskspace-frontend/src/app/kanban/board/board.ts
+++ taskspace-frontend/src/app/kanban/board/board.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Column, Task } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board.html',
-  styleUrl: './board.scss',
+  styleUrls: ['./board.scss'],
 })
-export class Board {}
+export class Board implements OnInit {
+  columns: Column[] = [];
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getColumns().subscribe((columns) => {
+      this.columns = columns;
+    });
+
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  getTasksForColumn(columnId: number): Task[] {
+    return this.tasks.filter((t) => t.columnId === columnId);
+  }
+}
