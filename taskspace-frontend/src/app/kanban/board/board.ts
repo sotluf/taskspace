e3aabd29fddeb88bdd7b4854762c5c2dbb9c9ea8
Oleksandra Maryanko <maryankoaleksandra@gmail.com>
2026-05-15 13:06:30 +0200

    refactor: simplify board component by using nested columns and cards

--- taskspace-frontend/src/app/kanban/board/board.ts
+++ taskspace-frontend/src/app/kanban/board/board.ts
@@ -1,31 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { Column, Task } from '../../models/task.model';
+import { Component } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { TaskService } from '../../services/task.service';
-import { CommonModule } from '@angular/common';
+import { Column as ColumnModel, Task } from '../../models/task.model';
+import { Column } from '../column/column';
+import { Observable, combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-board',
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe, Column],
   templateUrl: './board.html',
   styleUrls: ['./board.scss'],
 })
-export class Board implements OnInit {
-  columns: Column[] = [];
-  tasks: Task[] = [];
+export class Board {
+  columns$: Observable<ColumnModel[]>;
+  tasks$: Observable<Task[]>;
 
-  constructor(private taskService: TaskService) {}
-
-  ngOnInit(): void {
-    this.taskService.getColumns().subscribe((columns) => {
-      this.columns = columns;
-    });
-
-    this.taskService.getTasks().subscribe((tasks) => {
-      this.tasks = tasks;
-    });
+  constructor(private taskService: TaskService) {
+    this.columns$ = this.taskService.getColumns();
+    this.tasks$ = this.taskService.getTasks();
   }
 
-  getTasksForColumn(columnId: number): Task[] {
-    return this.tasks.filter((t) => t.columnId === columnId);
+  getTasksForColumn(tasks: Task[], columnId: number): Task[] {
+    return tasks.filter(t => t.columnId === columnId);
   }
 }
